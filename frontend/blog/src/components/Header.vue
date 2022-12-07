8986080e1285338939c0b8ab1dfd8932ad9165e4
duanqianwen <duanqingwen@xiyiqq.com>
2022-12-07 10:31:53 +0800

    博客页面

---
+++ frontend/blog/src/components/Header.vue
@@ -0,0 +1,31 @@
+<!--  -->
+<template>
+  <div id="header">
+        <div id="nav">
+            <div id="log">
+                <img src="../assets/image/logo/logo.png" />
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+</script>
+<style  scoped>
+
+</style>
---
+++ frontend/blog/src/views/blog/TagList.vue
@@ -0,0 +1,25 @@
+<!-- 标签展示 -->
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'TagList',
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted: {},
+
+  methods: {}
+}
+
+</script>
+<style lang='scss' scoped>
+</style>
